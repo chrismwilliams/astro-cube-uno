561e66025a0f635f57cccabfb245aa2b7c224a9c
Chris Williams <chrismwilliams@users.noreply.github.com>
2025-02-12 15:52:49 +0000

    add cube.css to uno

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,6 @@
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
 import { defineConfig, transformerDirectives, presetMini } from "unocss";
-// import presetWind from "@unocss/preset-wind";
 import {
   tokensToTailwind as tokensToUno,
   clampGenerator,
@@ -22,11 +23,32 @@ const spacing = tokensToUno(clampGenerator(spacingTokens.items, viewports));
 const fontSize = tokensToUno(clampGenerator(textSizeTokens.items, viewports));
 const lineHeight = tokensToUno(textLeadingTokens.items);
 
-// ! TODO: Add CUBE.css
+const resetCSS = await readFile(resolve("./src/css/global/reset.css"), "utf-8");
+const globalCSS = await readFile(
+  resolve("./src/css/global/global-styles.css"),
+  "utf-8"
+);
+const fontCSS = await readFile(
+  resolve("./src/css/global/font-face.css"),
+  "utf-8"
+);
+// ! TODO: Remove some styles from astro-cube, Compare
 
 export default defineConfig({
   transformers: [transformerDirectives()],
   presets: [presetMini({ preflight: "on-demand" })],
+  layers: {
+    reset: -1,
+    base: 1,
+    components: 2,
+    default: 3,
+  },
+  outputToCssLayers: {
+    cssLayerName: (layer) => {
+      if (layer === "preflights" || layer === "default") return null;
+      return layer;
+    },
+  },
   theme: {
     colors,
     fontFamily: Object.fromEntries(
@@ -46,6 +68,18 @@ export default defineConfig({
     },
   },
   preflights: [
+    {
+      layer: "reset",
+      getCSS: () => resetCSS,
+    },
+    {
+      layer: "base",
+      getCSS: () => globalCSS,
+    },
+    {
+      layer: "base",
+      getCSS: () => fontCSS,
+    },
     {
       layer: "base",
       getCSS: () => {
@@ -88,6 +122,141 @@ export default defineConfig({
     [
       /^gutter-(.+)$/,
       ([, d]) => ({ "--gutter": `var(--space-${d})` }),
+      { layer: "components" },
+    ],
+    [
+      /^flow$/,
+      (_, { symbols }) => {
+        return {
+          [symbols.selector]: (selector) => `${selector} > * + *`,
+          "margin-top": "var(--flow-space, 1em)",
+        };
+      },
+      { layer: "components" },
+    ],
+    [
+      "cluster",
+      {
+        display: "flex",
+        "flex-wrap": "wrap",
+        gap: "var(--cluster-gap, 1rem)",
+      },
+      { layer: "components" },
+    ],
+    [
+      "wrapper",
+      {
+        "margin-inline": "auto",
+        "max-width": "clamp(16rem, var(--wrapper-max-width, 100vw), 80rem)",
+        "padding-left": "var(--gutter)",
+        "padding-right": "var(--gutter)",
+        position: "relative",
+      },
+      { layer: "components" },
+    ],
+    [
+      "region",
+      {
+        "padding-block": "var(--region-space, var(--space-xl-2xl))",
+      },
+      { layer: "components" },
+    ],
+    [
+      "visually-hidden",
+      {
+        border: "0",
+        clip: "rect(0 0 0 0)",
+        height: "0",
+        margin: "0",
+        overflow: "hidden",
+        padding: "0",
+        position: "absolute",
+        width: "1px",
+        "white-space": "nowrap",
+      },
+      { layer: "components" },
+    ],
+    [
+      /^switcher$/,
+      function* (_, { symbols }) {
+        yield {
+          display: "flex",
+          "flex-wrap": "wrap",
+          gap: "var(--gutter, var(--space-s-l))",
+          "align-items": "var(--switcher-vertical-alignment, flex-start)",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector} > *`,
+          "flex-grow": "1",
+          "flex-basis":
+            "calc((var(--switcher-target-container-width, 40rem) - 100%) * 999)",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector} > :nth-child(n + 3)`,
+          "flex-basis": "100%",
+        };
+      },
+      { layer: "components" },
+    ],
+    [
+      /^sidebar$/,
+      function* (_, { symbols }) {
+        yield {
+          display: "flex",
+          "flex-wrap": "wrap",
+          gap: "var(--gutter, var(--space-s-l))",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector} > :first-child`,
+          "flex-grow": "1",
+          "flex-basis": "var(--sidebar-target-width, 20rem)",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector} > :last-child`,
+          "flex-basis": 0,
+          "flex-grow": 999,
+          "min-width": "var(--sidebar-content-min-width, 50%)",
+        };
+      },
+      { layer: "components" },
+    ],
+    [
+      /^repel$/,
+      function* (_, { symbols }) {
+        yield {
+          display: "flex",
+          "flex-wrap": "wrap",
+          gap: " var(--gutter, var(--space-s-m))",
+          "justify-content": "space-between",
+          "align-items": "var(--repel-vertical-alignment, center)",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector}[data-nowrap]`,
+          "flex-wrap": "nowrap",
+        };
+      },
+      { layer: "components" },
+    ],
+    [
+      /^grid$/,
+      function* (_, { symbols }) {
+        yield {
+          display: "grid",
+          "grid-template-columns":
+            "repeat(var(--grid-placement, auto-fill),minmax(var(--grid-min-item-size,16rem), 1fr))",
+          gap: "var(--gutter, var(--space-s-l))",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector}[data-layout="50-50"]`,
+          "--grid-placement": "auto-fit",
+          "--grid-min-item-size": "clamp(16rem, 50vw, 33rem)",
+        };
+        yield {
+          [symbols.selector]: (selector) => `${selector}[data-layout="thirds"]`,
+          "--grid-placement": "auto-fit",
+          "--grid-min-item-size": "clamp(16rem, 33%, 20rem)",
+        };
+      },
       { layer: "components" },
     ],
   ],
